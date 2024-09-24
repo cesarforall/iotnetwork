4c83b4e024560e5b592d6512a3e9a0635d176566
Cesar Almeida <cesaralmeida89@gmail.com>
2024-09-24 22:24:41 +0200

    Add secret key, database and instance path config

--- raspberrypi/iotserver/__init__.py
+++ raspberrypi/iotserver/__init__.py
@@ -3,7 +3,19 @@ import os
 from flask import Flask
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, instance_relative_config=True)
+
+    # Se pasa un diccionario para configurar como clave-valor
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'iotserver.db')
+    )
+
+    try:
+        # Crea el directorio "instance" donde se almacenan datos sensibles
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
 
     from . import api
     app.register_blueprint(api.bp)
